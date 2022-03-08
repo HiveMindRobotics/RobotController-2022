122c781be9a604120c6fd28a3fa586deae5b73ef
cheru-dev <mail@cheru.dev>
2022-03-07 20:04:11 -0500

    vuforiaaaaaa

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vuforia.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vuforia.kt
@@ -1,22 +1,19 @@
 package org.firstinspires.ftc.teamcode
 
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix
 import org.firstinspires.ftc.robotcore.external.ClassFactory
 import org.firstinspires.ftc.robotcore.external.navigation.*
 import java.util.ArrayList
 
-@TeleOp(name = "Vuforia Field Nav Webcam", group = "Concept")
-class ConceptVuforiaFieldNavigationWebcam : LinearOpMode() {
-    // Class Members
+class Vuforia(private val robot: Hardware, private val opMode: LinearOpMode) {
     private var lastLocation: OpenGLMatrix? = null
     private var vuforia: VuforiaLocalizer? = null
     private var targets: VuforiaTrackables? = null
     private var targetVisible = false
-    override fun runOpMode() {
-        val robot = Hardware()
-        robot.init(hardwareMap)
+    private var allTrackables: MutableList<VuforiaTrackable?>? = null
+
+    fun init() {
 
         val parameters = VuforiaLocalizer.Parameters()
         parameters.vuforiaLicenseKey = VUFORIA_KEY
@@ -29,8 +26,8 @@ class ConceptVuforiaFieldNavigationWebcam : LinearOpMode() {
 
         targets = vuforia!!.loadTrackablesFromAsset("FreightFrenzy")
 
-        val allTrackables: MutableList<VuforiaTrackable?> = ArrayList()
-        allTrackables.addAll(targets!!)
+        allTrackables = ArrayList()
+        allTrackables?.addAll(targets!!)
 
         identifyTarget(0, "Blue Storage", -halfField, oneAndHalfTile, mmTargetHeight, 90f, 0f, 90f)
         identifyTarget(1, "Blue Alliance Wall", halfTile, halfField, mmTargetHeight, 90f, 0f, 0f)
@@ -55,54 +52,52 @@ class ConceptVuforiaFieldNavigationWebcam : LinearOpMode() {
                 )
             )
 
-        for (trackable in allTrackables) {
+        for (trackable in allTrackables!!) {
             (trackable!!.listener as VuforiaTrackableDefaultListener).setCameraLocationOnRobot(
                 parameters.cameraName!!,
                 cameraLocationOnRobot
             )
         }
+    }
 
-        waitForStart()
+    fun getPosition() {
+        targets!!.activate()
 
-         targets!!.activate()
-        while (!isStopRequested) {
+        targetVisible = false
+        for (trackable in allTrackables!!) {
+            if ((trackable!!.listener as VuforiaTrackableDefaultListener).isVisible) {
+                opMode.telemetry.addData("Visible Target", trackable.name)
+                targetVisible = true
 
-            targetVisible = false
-            for (trackable in allTrackables) {
-                if ((trackable!!.listener as VuforiaTrackableDefaultListener).isVisible) {
-                    telemetry.addData("Visible Target", trackable.name)
-                    targetVisible = true
-
-                    val robotLocationTransform =
-                        (trackable.listener as VuforiaTrackableDefaultListener).updatedRobotLocation
-                    if (robotLocationTransform != null) {
-                        lastLocation = robotLocationTransform
-                    }
-                    break
+                val robotLocationTransform =
+                    (trackable.listener as VuforiaTrackableDefaultListener).updatedRobotLocation
+                if (robotLocationTransform != null) {
+                    lastLocation = robotLocationTransform
                 }
+                break
             }
-
-            if (targetVisible) {
-                val translation = lastLocation!!.translation
-                telemetry.addData(
-                    "Pos (inches)", "{X, Y, Z} = %.1f, %.1f, %.1f",
-                    translation[0] / mmPerInch, translation[1] / mmPerInch, translation[2] / mmPerInch
-                )
-
-                val rotation =
-                    Orientation.getOrientation(lastLocation, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES)
-                telemetry.addData(
-                    "Rot (deg)",
-                    "{Roll, Pitch, Heading} = %.0f, %.0f, %.0f",
-                    rotation.firstAngle,
-                    rotation.secondAngle,
-                    rotation.thirdAngle
-                )
-            } else {
-                telemetry.addData("Visible Target", "none")
-            }
-            telemetry.update()
         }
+
+        if (targetVisible) {
+            val translation = lastLocation!!.translation
+            opMode.telemetry.addData(
+                "Pos (inches)", "{X, Y, Z} = %.1f, %.1f, %.1f",
+                translation[0] / mmPerInch, translation[1] / mmPerInch, translation[2] / mmPerInch
+            )
+
+            val rotation =
+                Orientation.getOrientation(lastLocation, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES)
+            opMode.telemetry.addData(
+                "Rot (deg)",
+                "{Roll, Pitch, Heading} = %.0f, %.0f, %.0f",
+                rotation.firstAngle,
+                rotation.secondAngle,
+                rotation.thirdAngle
+            )
+        } else {
+            opMode.telemetry.addData("Visible Target", "none")
+        }
+        opMode.telemetry.update()
 
         targets!!.deactivate()
     }
@@ -133,7 +128,8 @@ class ConceptVuforiaFieldNavigationWebcam : LinearOpMode() {
     }
 
     companion object {
-        private const val VUFORIA_KEY = "Aci0vMT/////AAABmfhdrnZAvEt1j4pdeLfUrh4Z/YsocsADwlvHAd2ynqR01+VTMT5eWXkdbqzLprLlpI9jlWycsNSv+Bj15CbyYi1Tjv9u+QJ4t+zSZS3U1M00UW59a8CrDCTFTlEvDmEhf5RAlTY2SNhqq8U+MVdAMw7BG5osJdaHuY2UwHIdp1hKr8bThBLWT3vTF0C5VymM9KrueBCbt1b8Da2gphEzaYTyR6t8f1Ssu66O3t+WGDbnErM7OJs9kl75JyemAa9ijRqQyl9S/T8carMWNO3URaTX+9SIGeW+Mk4wKH7eKYPhXqoRb1jKULv2w0bQAU4Zz0K8DlcPtLT005iR9nHvSC5yzz99qgsLw/k3diSduCfV"
+        private const val VUFORIA_KEY =
+            "Aci0vMT/////AAABmfhdrnZAvEt1j4pdeLfUrh4Z/YsocsADwlvHAd2ynqR01+VTMT5eWXkdbqzLprLlpI9jlWycsNSv+Bj15CbyYi1Tjv9u+QJ4t+zSZS3U1M00UW59a8CrDCTFTlEvDmEhf5RAlTY2SNhqq8U+MVdAMw7BG5osJdaHuY2UwHIdp1hKr8bThBLWT3vTF0C5VymM9KrueBCbt1b8Da2gphEzaYTyR6t8f1Ssu66O3t+WGDbnErM7OJs9kl75JyemAa9ijRqQyl9S/T8carMWNO3URaTX+9SIGeW+Mk4wKH7eKYPhXqoRb1jKULv2w0bQAU4Zz0K8DlcPtLT005iR9nHvSC5yzz99qgsLw/k3diSduCfV"
 
         private const val mmPerInch = 25.4f
         private const val mmTargetHeight = 6 * mmPerInch // the height of the center of the target image above the floor
