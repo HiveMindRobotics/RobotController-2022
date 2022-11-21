0997655790d676d8773ad1b6372a465b5118b27e
NAHTAIV3L <rbeckettvt@gmail.com>
2022-11-20 21:23:21 -0500

    fixed bad comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.kt
@@ -80,14 +80,13 @@ class DriverControl : LinearOpMode() {
                         // linear slide
                         robot.motorLinearSlide.power = gamepad1.right_stick_y.toDouble()
 
+                        // Grabber - X to toggle open and close
                         if(gamepad2.x && !prevGamepad2.x)
                             toggleGrab = !toggleGrab
 
-                        // Grabber - X to close, Y to open
                         if(toggleGrab)
                             robot.grabberServo.position = 0.0
                         else
-                            // change to 0.3 later
                             robot.grabberServo.position = 0.3
 
                         // DRY - Don't Repeat Yourself   -- sky
